295b7a1c474f9e843a0f2d37a013e19a8d2923e6
adam <114612180+izvex309@users.noreply.github.com>
2022-09-28 22:07:00 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from time import sleep
+import os
+
+
+
+
+
+
+
+
+
+
+
+print(r"""
+███╗░░░███╗░█████╗░  ░█████╗░██████╗░██╗██████╗░░█████╗░░██████╗
+████╗░████║██╔══██╗  ██╔══██╗██╔══██╗██║██╔══██╗██╔══██╗██╔════╝
+██╔████╔██║██║░░╚═╝  ███████║██║░░██║██║██║░░██║███████║╚█████╗░
+██║╚██╔╝██║██║░░██╗  ██╔══██║██║░░██║██║██║░░██║██╔══██║░╚═══██╗
+██║░╚═╝░██║╚█████╔╝  ██║░░██║██████╔╝██║██████╔╝██║░░██║██████╔╝
+╚═╝░░░░░╚═╝░╚════╝░  ╚═╝░░╚═╝╚═════╝░╚═╝╚═════╝░╚═╝░░╚═╝╚═════╝░ """)
+
+while True:
+    d1a = input("menu:mc adidas A mc nuggets B mc sprate C ")
+    if d1a == "A":
+        print('ok your order will arive in 15 minutes ')
+    elif d1a == "B":
+        print('ok your order will arive in 15 minutes ')
+    elif d1a == "C":
+        print('ok your order will arive in 15 minutes ')
+    break
+
+input('\n')
+print('your wellcum')
+
+os.system('python mane.py')
+
+sleep(50)
+
+
+
+
